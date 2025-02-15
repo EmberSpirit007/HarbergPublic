a9d62456e309e649b8fde899db4917c56c9596c5
EmberSpirit007 <148816965+EmberSpirit007@users.noreply.github.com>
2025-02-15 16:59:22 +0100

    no lazyloading new build

--- assets/index-BhBb6iUV.js
+++ assets/index-BhBb6iUV.js
@@ -1,4 +1,4 @@
-import{T as Pe,M as nt,R as k,E as q,C as O,a as re,b as fo,O as K,c as fe}from"./index-CQcGzjET.js";import{O as mo}from"./index-C1P5wpvv.js";/**
+import{T as Pe,M as nt,R as k,E as q,C as O,a as re,b as fo,O as K,c as fe}from"./index-CvRYI2IK.js";import{ap as mo}from"./index-BQV0zcBE.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
